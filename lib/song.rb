f49cad5f27432135a4051193bc9341d75bdfdbd9
Dakota Clark <bannaray95@gmail.com>
2018-11-16 04:39:43 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -39,4 +39,13 @@ class Song
   def self.artist_count
     @@artists.inject(Hash.new(0)) { |total, i| total[i] += 1 ;total}
   end
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @@artists << artist
+    @genre = genre
+    @@genres << genre
+    @@count += 1
+  end
+  attr_accessor :name, :artist, :genre
 end
